a6b2b7b4dc11f86fc01448d77978a387d322072f
Gergely Gyevi Szabo <ggyevi-s@c2r10s7.42prague.com>
2023-06-13 18:38:03 +0200

    hajra

--- Brainsnippets/42-workshops/c01-workshop/ft_swap.c
+++ Brainsnippets/42-workshops/c01-workshop/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: ggyevi-s <ggyevi-s@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 16:42:45 by ggyevi-s          #+#    #+#             */
-/*   Updated: 2023/06/12 16:59:40 by ggyevi-s         ###   ########.fr       */
+/*   Updated: 2023/06/13 18:02:57 by ggyevi-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,25 @@ void	ft_swap(int *a, int *b)
 	c = *a;
 	*a = *b;
 	*b = c;
-	printf("int %i\n", *a);
-	printf("int %i\n", *b);
 }
 
 int	main(void)
 {
 	int x;
 	int y;
+	int c;
 	x = 4;
 	y = 2;
+	
+	
+	char x1;
+	char y2;
+	x1 = y + '0';
+	y2 = x + '0';
+	
 	ft_swap(&x, &y);
+
+	write(1, &x1, 1);
+	write(1, "\n", 1);
+	write(1, &y2, 1);
 }
