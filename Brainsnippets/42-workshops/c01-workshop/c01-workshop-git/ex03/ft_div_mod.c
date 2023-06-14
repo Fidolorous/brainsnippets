48bca717bd5e4dee608c5bddfe5a77b37236a619
fidolorous <fidolorous@hotmail.com>
2023-06-14 13:40:11 +0200

    nigi

--- Brainsnippets/42-workshops/c01-workshop/c01-workshop-git/ex03/ft_div_mod.c
+++ Brainsnippets/42-workshops/c01-workshop/c01-workshop-git/ex03/ft_div_mod.c
@@ -6,29 +6,32 @@
 /*   By: ggyevi-s <ggyevi-s@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 12:25:25 by ggyevi-s          #+#    #+#             */
-/*   Updated: 2023/06/14 13:00:22 by ggyevi-s         ###   ########.fr       */
+/*   Updated: 2023/06/14 13:08:44 by ggyevi-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 
-void    ft_div_mod(int a, int b, int *div, int *mod)
+void	ft_div_mod(int a, int b, int *div, int *mod)
 
 {
-	if(b != '0') //same as (b) 
+	if (b != '0')
 	{
 		*div = a / b;
 		*mod = a % b;
 	}
 }
-
-int	main()
+/*
+int	main(void)
 {
-	int	div, mod, n, n1;
-	
+	int	div;
+	int	mod;
+	int	n;
+	int	n1;
+
 	n = 42;
 	n1 = 21;
-
 	ft_div_mod(n, n1, &div, &mod);
 	printf("\n%d/%d = %d with remainder %d\n\n", n, n1, div, mod);
 }
+*/
