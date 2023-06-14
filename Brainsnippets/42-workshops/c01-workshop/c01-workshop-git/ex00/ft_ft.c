d76a3fab9d165e5c8a83822052ed9ef061889eb5
Gergely Gyevi Szabo <ggyevi-s@c2r10s1.42prague.com>
2023-06-14 11:50:46 +0200

    asdasd

--- Brainsnippets/42-workshops/c01-workshop/c01-workshop-git/ex00/ft_ft.c
+++ Brainsnippets/42-workshops/c01-workshop/c01-workshop-git/ex00/ft_ft.c
@@ -6,29 +6,39 @@
 /*   By: ggyevi-s <ggyevi-s@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 12:35:56 by ggyevi-s          #+#    #+#             */
-/*   Updated: 2023/06/13 23:25:16 by ggyevi-s         ###   ########.fr       */
+/*   Updated: 2023/06/14 10:04:06 by ggyevi-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include<stdio.h>
 #include<unistd.h>
 
 void	ft_ft(int *nbr)
 {
 	*nbr = 42;
 }
-
+/*
 int	main(void)
-{
+{	
 	int	x;
-	x = 33;
 	int	low;
 	int	high;
-	low = x%10;
-	high = x/10;
-	char	putchar;	
-	putchar(low + 0x30);
+	int	*nbr;
+
+	nbr = &x;
+	x = 11;
+	low = x % 10;
+	high = x / 10;
+	x = 11;
+	printf("%i \n", x);
+	ft_ft(nbr);
+	printf("%i", x);
+}
+
+	putchar(low+0x30);
+	putchar(high+'0');
 
 	ft_ft (&x);
-	char buf[]= {'0'+(x/10)%10, '0'+x%10};
-	write(1,buf,2);
-}
+	char buf[22]= {'0'+(x/10)%10, '0'+x%10, '\n'};
+	write(1, buf, 3);
+*/
