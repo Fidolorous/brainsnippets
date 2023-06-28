3a29cf0e0b3957b7d22ca9ef54feeb479f7976ac
Gergely Gyevi Szabo <ggyevi-s@c1r6s1.42prague.com>
2023-06-28 20:59:21 +0200

    oceano

--- Brainsnippets/42-workshops/c03-workshop/ft_strcmp.c
+++ Brainsnippets/42-workshops/c03-workshop/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ggyevi-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 19:08:16 by ggyevi-s          #+#    #+#             */
-/*   Updated: 2023/06/28 19:58:53 by ggyevi-s         ###   ########.fr       */
+/*   Updated: 2023/06/28 20:27:13 by ggyevi-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,37 @@
 
 #include <unistd.h>
 #include <stdio.h>
-
+#include <string.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
+	while	(*s1 == *s2 && *s1)
+		++s1, ++s2;
 
+	return (*s1 - *s2);
+}
+
+/*
+int main()
+{
+	char	*s; 
+	char	*s1; 
+
+	s = "Hello";
+	s1 = "Hello";
+	printf("%s\n%s\nft_strcmp-> %d\nstrcmp->    %d\n\n\n", s, s1, ft_strcmp(s, s1), strcmp(s, s1));
+
+	s = "hello";
+	s1 = "hel";
+	printf("%s\n%s\nft_strcmp-> %d\nstrcmp->    %d\n\n\n", s, s1, ft_strcmp(s, s1), strcmp(s, s1));
+
+	s = "hel";
+	s1 = "hello";
+	printf("%s\n%s\nft_strcmp-> %d\nstrcmp->    %d\n\n\n", s, s1, ft_strcmp(s, s1), strcmp(s, s1));
+
+
+	s = "";
+	s1 = "";
+	printf("\"%s\"\n\"%s\"\nft_strcmp-> %d\nstrcmp->    %d\n\n\n", s, s1, ft_strcmp(s, s1), strcmp(s, s1));
+}
+*/
